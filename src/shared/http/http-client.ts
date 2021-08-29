7dd967d26f50a7d812b220ed678b18fb62d3e2d1
Tim Pequignot <tim@peq.me>
2021-08-28 22:25:35 -0500

    Replace axios with fetch

--- src/shared/http/http-client.ts
+++ src/shared/http/http-client.ts
@@ -1,18 +1,10 @@
 export default class HttpClient {
-    private _callApiSIDToken: string;
-    private _apiToken: string;
+    readonly callApiSIDToken: string;
+    readonly apiToken: string;
 
     constructor(callApiSIDToken: string, apiToken: string) {
-        this._callApiSIDToken = callApiSIDToken;
-        this._apiToken = apiToken;
-    }
-
-    get callApiSIDToken(): string {
-        return this._callApiSIDToken;
-    }
-
-    get apiToken(): string {
-        return this._apiToken;
+        this.callApiSIDToken = callApiSIDToken;
+        this.apiToken = apiToken;
     }
 
     public async fetch<T>(
@@ -21,7 +13,6 @@ export default class HttpClient {
         data: any = {},
         additionalConfig: Partial<RequestInit> = {},
     ): Promise<T> {
-        console.log(method)
         const config: RequestInit = {
             method: method,
             ...additionalConfig,
@@ -29,21 +20,14 @@ export default class HttpClient {
 
         Object.keys(data).forEach((key) => {
             if (data[key] === null || data[key] === '') {
+                console.debug(`http fetch is removing blank or null data. Key: ${key}  Data: ${data}`);
                 delete data[key];
             }
         });
 
-        // if (typeof data === "object") {
-        //     config.params = {
-        //         ...config.params,
-        //         ...data,
-        //     };
-        // }
-        // config.data = data;
-
         var fetchURL = new URL(url);
         fetchURL.search = new URLSearchParams({token:this.apiToken}).toString();
-        console.log(fetchURL.search);
+    
         const response = await fetch(fetchURL, config);
 
         return response.json();
